7f995d2d3c5590c1d56d13040c207be6a02c8154
matthewtregg <tregearmatthew@gmail.com>
2019-06-28 17:41:20 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,1 @@
-function tribonacci(startingInput,n){  
-  for (let i = 0; i < n-3; i++) { // iterate n times
-    startingInput.push(startingInput[i] + startingInput[i+1] + startingInput[i+2]); // add last 3 array items and push to trib
-  }
-  return startingInput.slice(0, n); //return trib - length of n
-}
+
