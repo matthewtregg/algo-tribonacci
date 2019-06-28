46da2bab0c4e3e9e19cb8bfc6f75b8badbab2d29
matthewtregg <tregearmatthew@gmail.com>
2019-06-28 19:47:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 // b) the length of the tribonacci series that should be returned
 // e.g. `tribonacci([1,1,1],10) ===> [1,1,1,3,5,9,17,31,57,105]`
 
+// NB please check the tests to make sure all cases are covered.  
+
 // TESTING YOUR SOLUTION
 
 // To test your solution, run 'npm install' in the root directory
@@ -16,6 +18,7 @@
 
 // When done, make sure you're working on a forked repo, push your
 // changes to your forked repo and submit a pull request.
+
 
 
 
